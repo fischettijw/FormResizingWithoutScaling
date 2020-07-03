1bfbf412cbcff798938b4a0d16fed4fee531131b
Joseph Fischetti <fischettijw@gmail.com>
2020-07-03 16:45:29 -0400

    1st on RightColumnButtons

--- FrmResize.Designer.cs
+++ FrmResize.Designer.cs
@@ -177,7 +177,7 @@
             // 
             this.LblUser.AutoSize = true;
             this.LblUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblUser.Location = new System.Drawing.Point(403, 35);
+            this.LblUser.Location = new System.Drawing.Point(404, 35);
             this.LblUser.Name = "LblUser";
             this.LblUser.Size = new System.Drawing.Size(89, 20);
             this.LblUser.TabIndex = 13;
